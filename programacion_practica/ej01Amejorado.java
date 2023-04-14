526cf2e13dac50db32b6f4c3aab1e8abe11ca0f7
Franco <7francogimenez@gmail.com>
2023-04-14 16:41:24 -0300

    practica de java

---
+++ programacion_practica/ej01Amejorado.java
@@ -0,0 +1,23 @@
+package programacion_practica;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+public class ej01Amejorado {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa tu fecha de nacimiento en formato dd/mm/aaaa: ");
+        String fechaNacimiento = sc.nextLine();
+        String[] fechaArray = fechaNacimiento.split("/");
+        int dia = Integer.parseInt(fechaArray[0]);
+        int mes = Integer.parseInt(fechaArray[1]);
+        int anio = Integer.parseInt(fechaArray[2]);
+        LocalDate fechaActual = LocalDate.now();
+        LocalDate fechaNac = LocalDate.of(anio, mes, dia);
+        Period periodo = Period.between(fechaNac, fechaActual);
+        System.out.println("Tu edad exacta es: " + periodo.getYears() + " años, " + periodo.getMonths() + " meses y " + periodo.getDays() + " días.");
+    }
+}
